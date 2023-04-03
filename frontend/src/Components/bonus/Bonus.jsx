7ae875ad253cbc92233458fd0d24a78af18ceffb
astoyanov98 <49924052+astoyanov98@users.noreply.github.com>
2023-04-03 08:38:07 -0700

    final commit

--- frontend/src/Components/bonus/Bonus.jsx
+++ frontend/src/Components/bonus/Bonus.jsx
@@ -3,14 +3,14 @@ import Badge from "../badge/Badge";
 import './Bonus.css'
 
 const Bonus = () => {
-    return(
+    return (
         <div className="container_bonus">
-            <Badge title='EXCLUSIVE'/>
-            <span className="title">Title</span>
+            <Badge title='EXCLUSIVE' />
+            <span className="title">Bonus</span>
             <div className="bonus">
                 <span>250 CA$</span>
             </div>
-            <span style={{fontSize:'20px'}}>+80 Tours Gratuits</span>
+            <span style={{ fontSize: '20px' }}>+80 Tours Gratuits</span>
         </div>
     )
 }
--- frontend/src/Components/button/Button.jsx
+++ frontend/src/Components/button/Button.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import './Button.css'
 
-const Button = ({label}) => {
-    return(
+const Button = ({ label }) => {
+    return (
         <div className="buttonContainer">
             <span>{label}</span>
         </div>
